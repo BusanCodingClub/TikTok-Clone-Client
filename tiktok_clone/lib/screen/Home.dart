b2ffc24702320e64f3183f3d4c71cc63ee2064f1
qodbtn41 <qodbtn41@naver.com>
2023-04-26 00:59:50 +0900

    Home 추가 - flutterbiz에서 만듦

---
+++ tiktok_clone/lib/screen/Home.dart
@@ -0,0 +1,306 @@
+///File download from FlutterViz- Drag and drop a tools. For more details visit https://flutterviz.io/
+
+///File download from FlutterViz- Drag and drop a tools. For more details visit https://flutterviz.io/
+
+import 'package:flutter/material.dart';
+import '../model/flutterViz_bottom_navigationBar_model.dart';
+import '../widget/flutterViz_video_player.dart';
+
+class Home extends StatelessWidget {
+  List<FlutterVizBottomNavigationBarModel> flutterVizBottomNavigationBarItems =
+      [
+    FlutterVizBottomNavigationBarModel(icon: Icons.home, label: "Home"),
+    FlutterVizBottomNavigationBarModel(icon: Icons.language, label: "Friends"),
+    FlutterVizBottomNavigationBarModel(icon: Icons.add_box, label: ""),
+    FlutterVizBottomNavigationBarModel(icon: Icons.mail, label: "Inbox"),
+    FlutterVizBottomNavigationBarModel(icon: Icons.person, label: "Profile")
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffffffff),
+      bottomNavigationBar: BottomNavigationBar(
+        items: flutterVizBottomNavigationBarItems
+            .map((FlutterVizBottomNavigationBarModel item) {
+          return BottomNavigationBarItem(
+            icon: Icon(item.icon),
+            label: item.label,
+          );
+        }).toList(),
+        backgroundColor: Color(0xff000000),
+        currentIndex: 0,
+        elevation: 8,
+        iconSize: 28,
+        selectedItemColor: Color(0xffffffff),
+        unselectedItemColor: Color(0x7f000000),
+        selectedFontSize: 9,
+        unselectedFontSize: 9,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        onTap: (value) {},
+      ),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          alignment: Alignment.topLeft,
+          children: [
+            Container(
+              margin: EdgeInsets.all(0),
+              padding: EdgeInsets.all(0),
+              width: MediaQuery.of(context).size.width,
+              height: 40,
+              decoration: BoxDecoration(
+                color: Color(0x00000000),
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.zero,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(0),
+                    padding: EdgeInsets.all(0),
+                    width: MediaQuery.of(context).size.width * 0.85,
+                    height: MediaQuery.of(context).size.height,
+                    decoration: BoxDecoration(
+                      color: Color(0x00000000),
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.zero,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        MaterialButton(
+                          onPressed: () {},
+                          color: Color(0x00ffffff),
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                          ),
+                          padding: EdgeInsets.all(16),
+                          child: Text(
+                            "Following",
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              fontStyle: FontStyle.normal,
+                            ),
+                          ),
+                          textColor: Color(0xff000000),
+                          height: MediaQuery.of(context).size.height,
+                          minWidth: 50,
+                        ),
+                        MaterialButton(
+                          onPressed: () {},
+                          color: Color(0x00ffffff),
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                          ),
+                          padding: EdgeInsets.all(16),
+                          child: Text(
+                            "For You",
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              fontStyle: FontStyle.normal,
+                            ),
+                          ),
+                          textColor: Color(0xff000000),
+                          height: 40,
+                          minWidth: 50,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(0),
+                    padding: EdgeInsets.all(0),
+                    width: MediaQuery.of(context).size.width * 0.1,
+                    height: MediaQuery.of(context).size.height,
+                    decoration: BoxDecoration(
+                      color: Color(0x00000000),
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.zero,
+                    ),
+                    child: Icon(
+                      Icons.search,
+                      color: Color(0xff212435),
+                      size: 24,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment(0.0, 0.7),
+              child: Container(
+                margin: EdgeInsets.all(0),
+                padding: EdgeInsets.all(0),
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                decoration: BoxDecoration(
+                  color: Color(0x00000000),
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.zero,
+                  border: Border.all(color: Color(0x4d9e9e9e), width: 1),
+                ),
+                child: FlutterVizVideoPlayer(
+                  url:
+                      "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4",
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                margin: EdgeInsets.all(0),
+                padding: EdgeInsets.all(0),
+                width: MediaQuery.of(context).size.width,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Color(0x00000000),
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.zero,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      margin: EdgeInsets.all(0),
+                      padding: EdgeInsets.all(0),
+                      width: MediaQuery.of(context).size.width * 0.6,
+                      height: MediaQuery.of(context).size.height,
+                      decoration: BoxDecoration(
+                        color: Color(0x00000000),
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.zero,
+                      ),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: EdgeInsets.all(4),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Text(
+                                "레시피 읽어주는 여자",
+                                textAlign: TextAlign.start,
+                                overflow: TextOverflow.clip,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontStyle: FontStyle.normal,
+                                  fontSize: 14,
+                                  color: Color(0xff000000),
+                                ),
+                              ),
+                              Text(
+                                "3:03 PM - Sharou",
+                                textAlign: TextAlign.left,
+                                overflow: TextOverflow.clip,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontStyle: FontStyle.normal,
+                                  fontSize: 14,
+                                  color: Color(0xff000000),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(0),
+                      padding: EdgeInsets.all(0),
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      height: MediaQuery.of(context).size.height,
+                      decoration: BoxDecoration(
+                        color: Color(0x00000000),
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.zero,
+                      ),
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        clipBehavior: Clip.antiAlias,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                        ),
+                        child: Image.network(
+                            "https://picsum.photos/250?image=9",
+                            fit: BoxFit.contain),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment(1.0, 0.7),
+              child: Container(
+                margin: EdgeInsets.all(0),
+                padding: EdgeInsets.all(0),
+                width: 40,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Color(0x00000000),
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.zero,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    ImageIcon(
+                      NetworkImage(
+                          "https://image.flaticon.com/icons/png/512/281/281764.png"),
+                      size: 32,
+                      color: Color(0xffffffff),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.favorite),
+                      onPressed: () {},
+                      color: Color(0xffffffff),
+                      iconSize: 24,
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.sms),
+                      onPressed: () {},
+                      color: Color(0xffffffff),
+                      iconSize: 24,
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.bookmark_outline),
+                      onPressed: () {},
+                      color: Color(0xffffffff),
+                      iconSize: 24,
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.launch),
+                      onPressed: () {},
+                      color: Color(0xffffffff),
+                      iconSize: 24,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
